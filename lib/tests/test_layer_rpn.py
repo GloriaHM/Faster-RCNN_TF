585b879d212bb95d0bcee328691479ddcebae7a7
Sai Zhang <zhangzsai@gmail.com>
2018-12-08 17:51:03 -0800

    added test for rpn anchor target layer

---
+++ lib/tests/test_layer_rpn.py
@@ -0,0 +1,63 @@
+import sys, os
+import unittest
+import numpy as np, cv2
+import logging
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import tensorlayer as tl
+from models.layer_rpn import RPN
+import pickle
+
+class Test_layer_rpn(unittest.TestCase):
+
+    def setUp(self):
+        self.datadir = 'tests/test_layer_rpn_data/'
+
+
+    def test_rpn_anchor_target_layer(self):
+        tf.reset_default_graph()
+        with open(self.datadir+'test_anchor_target.pkl', 'rb') as fid:
+            df = pickle.load(fid)
+
+        with open(self.datadir+'/test_anchor_target_refout.pkl', 'rb') as fid:
+            res_ref = pickle.load(fid)
+
+        gt_boxes = df['gt_boxes']
+        im_info = df['im_info']
+        rpn_cls_score = df['rpn_cls_score']
+
+        rpn_labels_ref = res_ref['rpn_labels']
+        rpn_bbox_targets_ref = res_ref['rpn_bbox_targets']
+        rpn_bbox_inside_weights_ref = res_ref['rpn_bbox_inside_weights']
+        rpn_bbox_outside_weights_ref = res_ref['rpn_bbox_outside_weights']
+
+        rpn = RPN(DEBUG = True)
+        l,b, iw, ow = rpn.rpn_anchor_target_layer(
+                tf.shape(rpn_cls_score),
+                gt_boxes,
+                im_info,
+                _feat_stride = [16,],
+                anchor_scales = [8, 16, 32],
+                name = 'rpn_anchor_target_layer'
+                )
+
+        with tf.Session() as sess:
+            label, boxtarget, iw, ow = sess.run([ l, b, iw, ow ])
+
+        print label, label, iw, ow
+
+        assert(np.array_equal(label, rpn_labels_ref) )
+        assert(np.array_equal(boxtarget, rpn_bbox_targets_ref) )
+        assert(np.array_equal(iw, rpn_bbox_inside_weights_ref) )
+        assert(np.array_equal(ow, rpn_bbox_outside_weights_ref) )
+
+
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(Test_layer_rpn('test_rpn_anchor_target_layer'))
+
+    unittest.TextTestRunner(verbosity = 2).run(suite)
+
+
