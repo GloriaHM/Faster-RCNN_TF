b894020fe035adaa8195f8b5bb400960f327a6df
Sai Zhang <zhangzsai@gmail.com>
2018-12-10 19:07:12 -0800

    passed all rpn tests

--- lib/models/layer_rpn.py
+++ lib/models/layer_rpn.py
@@ -3,26 +3,111 @@ from fast_rcnn.config import cfg
 import numpy.random as npr
 from generate_anchors import generate_anchors
 from utils.cython_bbox import bbox_overlaps
-from fast_rcnn.bbox_transform import bbox_transform
+from fast_rcnn.bbox_transform import bbox_transform, bbox_transform_inv, clip_boxes
+from fast_rcnn.nms_wrapper import nms
+
 import os
 import tensorflow as tf
 
 
 class RPN(object):
 
-    def __init__( self ,DEBUG = False):
+    def __init__(self, DEBUG = False):
         self.DEBUG = DEBUG
 
+    def create( self ,
+            features ,
+            im_info,
+            gt_boxes = None,
+            isTrain = False,
+            ):
+        '''
+        input:
+            features - None , h , w , (NscalexNratiox2)
+            gt_boxes - Ngt , 4
+            im_info - contains information about original image size
 
-    def get_outputs(self ):
-        pass
+        output:
+            proposals - (Nprop , 5)
+            rpn_cross_entropy
+            rpn_loss_box
 
-    def rpn_head(self ):
+        high level connections:
+        featuers                            fe_shape, gt_boxes, im_info
+           |                                            |
+        [  rpn_head  ]                      [  rpn_anchor_target_layer ]
+         |                                              |
+        rpn_cls_score, rpn_bbox_pred        rpn_labels,rpn_bbox_targets,rpn_bbox_iw,rpn_bbox_ow
+         |                            \                       |
+        [  rpn_cls_score_reshape  ]    \_________________     |
+          |                             |               |     |
+        rpn_cls_prob_reshape, rpn_cls_sc|ore_reshape    |     |
+                  |                    /        |       |     |
+                  |              _____/     [       rpn_loss           ]
+                  |              |                      |
+        [   proposal_layer         ]                cross_entropy_loss and bbox_loss
+                  |
+            proposal blobs
+        '''
+        #high level configs here
+        _feat_stride = [16,]
+        anchor_scales = [8, 16, 32]
+
+        # data path definition
+        rpn_cls_score, rpn_bbox_pred = \
+            self.rpn_head(features, nfilters = 512, nscales = len(anchor_scales) )
+
+        rpn_cls_score_reshape, rpn_cls_prob_reshape \
+                = self.rpn_cls_score_reshape( rpn_cls_score )
+
+        cfg_key = 'TRAIN' if isTrain else 'TEST'
+        self.proposals = self.proposal_layer( rpn_cls_prob_reshape,
+            rpn_bbox_pred, im_info, cfg_key, _feat_stride, anchor_scales
+            )
+
+        #training specific
+        rpn_labels,rpn_bbox_targets,rpn_bbox_iw,rpn_bbox_ow = \
+                self.rpn_anchor_target_layer( tf.shape(features),
+                gt_boxes,
+                im_info,
+                _feat_stride,
+                anchor_scales, "rpn_anchor_target_layer")
+
+        self.rpn_cross_entropy, self.rpn_loss_box = \
+                self.rpn_loss(
+                rpn_cls_score_reshape, rpn_bbox_pred,
+                rpn_labels, rpn_bbox_targets, rpn_bbox_iw, rpn_bbox_ow)
+
+        return self
+
+    def get_outputs(self):
+
+        return self.proposals, self.rpn_cross_entropy, self.rpn_loss_box
+
+    def rpn_head(self, features, nfilters, nscales= 3, nratios = 3 ):
         '''
         input: features - None , h , w , 512
-        output: rpn_cls_score - None , h , w , (NscalexNanchorx2)
-                rpn_bbox_pred - None , h , w , (NscalexNanchorx4)
+        output: rpn_cls_score - None , h , w , (NscalexNratiox2)
+                rpn_bbox_pred - None , h , w , (NscalexNratiox4)
         '''
+        rpn_conv33 = tf.layers.conv2d(
+                features,
+                nfilters,
+                kernel_size = 3,
+                padding = 'SAME', activation = tf.nn.relu, name = 'rpn_conv/3x3')
+
+        rpn_cls_score = tf.layers.conv2d(
+                rpn_conv33,
+                2*nscales*nratios,
+                kernel_size = 1, padding = 'VALID', name = 'rpn_cls_score' )
+
+        rpn_bbox_pred =  tf.layers.conv2d(
+                rpn_conv33,
+                4*nscales*nratios,
+                kernel_size = 1, padding = 'VALID', name = 'rpn_bbox_pred' )
+
+        return rpn_cls_score, rpn_bbox_pred
+
 
     def rpn_anchor_target_layer(self, fe_shape, gt_boxes, im_info, _feat_stride, anchor_scales, name):
         '''
@@ -294,21 +379,60 @@ class RPN(object):
         return rpn_labels,rpn_bbox_targets,rpn_bbox_inside_weights,rpn_bbox_outside_weights
 
 
-
-    def rpn_loss(self):
+    def rpn_loss(self,
+        rpn_cls_score, rpn_bbox_pred,
+        rpn_labels, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights):
         '''
         input:
-            rpn_cls_score
-            rpn_bbox_pred
-            gt_boxes
+            rpn_cls_score_reshape - [1, 9h, w, 2]
+            rpn_bbox_pred - [None , h , w , (NscalexNratiox4)]
+
+            rpn_labels - (1, 1, Nanchor (e.g.3x3) * height, width)
+            rpn_bbox_targets - (1 , 4*Nanchor , height, width)
+            rpn_bbox_inside_weights - (1 , 4*Nanchor , height, width)
+            rpn_bbox_outside_weights -  (1 , 4*Nanchor , height, width)
         output:
-            rpn_label_loss
-            rpn_bbox_loss
+            rpn_cross_entropy
+            rpn_loss_box
         '''
-        pass
 
+        def _modified_smooth_l1( sigma, bbox_pred, bbox_targets, bbox_inside_weights, bbox_outside_weights):
+            """
+            ResultLoss = outside_weights * SmoothL1(inside_weights * (bbox_pred - bbox_targets))
+            SmoothL1(x) = 0.5 * (sigma * x)^2,    if |x| < 1 / sigma^2
+                          |x| - 0.5 / sigma^2,    otherwise
+            """
+            sigma2 = sigma * sigma
 
-    def _rpn_cls_score_reshape(self, rpn_cls_score):
+            inside_mul = tf.multiply(bbox_inside_weights, tf.subtract(bbox_pred, bbox_targets))
+
+            smooth_l1_sign = tf.cast(tf.less(tf.abs(inside_mul), 1.0 / sigma2), tf.float32)
+            smooth_l1_option1 = tf.multiply(tf.multiply(inside_mul, inside_mul), 0.5 * sigma2)
+            smooth_l1_option2 = tf.subtract(tf.abs(inside_mul), 0.5 / sigma2)
+            smooth_l1_result = tf.add(tf.multiply(smooth_l1_option1, smooth_l1_sign),
+                                      tf.multiply(smooth_l1_option2, tf.abs(tf.subtract(smooth_l1_sign, 1.0))))
+
+            outside_mul = tf.multiply(bbox_outside_weights, smooth_l1_result)
+
+            return outside_mul
+
+        rpn_cls_score = tf.reshape( rpn_cls_score , [-1,2] )
+        rpn_label = tf.reshape(rpn_labels,[-1])
+        rpn_cls_score = tf.reshape(tf.gather(rpn_cls_score,tf.where(tf.not_equal(rpn_label,-1))),[-1,2])
+        rpn_label = tf.reshape(tf.gather(rpn_label,tf.where(tf.not_equal(rpn_label,-1))),[-1])
+        rpn_cross_entropy = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=rpn_cls_score, labels=rpn_label), name = 'rpn_cross_entropy')
+
+        # bounding box regression L1 loss
+        rpn_bbox_targets = tf.transpose(rpn_bbox_targets,[0,2,3,1])
+        rpn_bbox_inside_weights = tf.transpose(rpn_bbox_inside_weights, [0,2,3,1])
+        rpn_bbox_outside_weights = tf.transpose(rpn_bbox_outside_weights ,[0,2,3,1])
+
+        rpn_smooth_l1 = _modified_smooth_l1(3.0, rpn_bbox_pred, rpn_bbox_targets, rpn_bbox_inside_weights, rpn_bbox_outside_weights)
+        rpn_loss_box = tf.reduce_mean(tf.reduce_sum(rpn_smooth_l1, reduction_indices=[1, 2, 3]), name = 'rpn_loss_box')
+
+        return rpn_cross_entropy, rpn_loss_box
+
+    def rpn_cls_score_reshape(self, rpn_cls_score):
         ''' apply softmax for rpn_cls_score
         input: rpn_cls_score - 1,h,w,2xNanchor
         output: rpn_cls_prob_reshape -  1,h,w,2xNanchor
@@ -322,7 +446,7 @@ class RPN(object):
         d = 2
         reshape1 = tf.transpose(
                 tf.reshape( tf.transpose(rpn_cls_score,[0,3,1,2]) , [batch, d, h*Nanchor2/d, w] ),
-                [0,2,3,1]
+                [0,2,3,1], name = 'rpn_csl_score_reshape'
                 )
 
         #softmax across last dim [1, 9h, w, 2]
@@ -333,12 +457,168 @@ class RPN(object):
                 tf.reshape(tf.transpose(reshape2,[0,3,1,2]), [batch, Nanchor2, h, w]),
                 [0,2,3,1],name='rpn_cls_prob_reshape')
 
-        return rpn_cls_prob_reshape
+        return reshape1, rpn_cls_prob_reshape
 
-    def proposal_layer(self):
+
+    def proposal_layer(self,
+            rpn_cls_prob_reshape,
+            rpn_bbox_pred,
+            im_info,
+            cfg_key,
+            _feat_stride = [16,],
+            anchor_scales = [8, 16, 32]):
         '''
         input:
+            rpn_cls_prob_reshape - 1,h,w,2xNanchor
+            rpn_bbox_pred - 1,h,w,4xNanchor where Nanchor = Nscale x Nratio
+            im_info - contains information about original image size
         output:
+
         '''
-        pass
+        return tf.reshape(tf.py_func(self._proposal_layer_py,
+            [rpn_cls_prob_reshape,rpn_bbox_pred,im_info, cfg_key, _feat_stride, anchor_scales],
+            [tf.float32]),[-1,5],name = "propoal_layer")
+
+
+    def _proposal_layer_py(self,
+            rpn_cls_prob_reshape,
+            rpn_bbox_pred,
+            im_info,
+            cfg_key,
+            _feat_stride = [16,],
+            anchor_scales = [8, 16, 32]):
+        '''
+        input:
+            rpn_cls_prob_reshape - 1,h,w,2xNanchor
+            rpn_bbox_pred - 1,h,w,4xNanchor where Nanchor = Nscale x Nratio
+            im_info - contains information about original image size
+        output:
+            blobs - Nrois x 5 (index, 4 bbox coordinates)
+        '''
+
+        def _filter_boxes(boxes, min_size):
+            """Remove all boxes with any side smaller than min_size."""
+            ws = boxes[:, 2] - boxes[:, 0] + 1
+            hs = boxes[:, 3] - boxes[:, 1] + 1
+            keep = np.where((ws >= min_size) & (hs >= min_size))[0]
+            return keep
+
+        # Algorithm:
+        #
+        # for each (H, W) location i
+        #   generate A anchor boxes centered on cell i
+        #   apply predicted bbox deltas at cell i to each of the A anchors
+        # clip predicted boxes to image
+        # remove predicted boxes with either height or width < threshold
+        # sort all (proposal, score) pairs by score from highest to lowest
+        # take top pre_nms_topN proposals before NMS
+        # apply NMS with threshold 0.7 to remaining proposals
+        # take after_nms_topN proposals after NMS
+        # return the top proposals (-> RoIs top, scores top)
+        #layer_params = yaml.load(self.param_str_)
+        _anchors = generate_anchors(scales=np.array(anchor_scales))
+        _num_anchors = _anchors.shape[0]
+        rpn_cls_prob_reshape = np.transpose(rpn_cls_prob_reshape,[0,3,1,2])
+        rpn_bbox_pred = np.transpose(rpn_bbox_pred,[0,3,1,2])
+        #rpn_cls_prob_reshape = np.transpose(np.reshape(rpn_cls_prob_reshape,[1,rpn_cls_prob_reshape.shape[0],rpn_cls_prob_reshape.shape[1],rpn_cls_prob_reshape.shape[2]]),[0,3,2,1])
+        #rpn_bbox_pred = np.transpose(rpn_bbox_pred,[0,3,2,1])
+        im_info = im_info[0]
+
+        assert rpn_cls_prob_reshape.shape[0] == 1, \
+            'Only single item batches are supported'
+        # cfg_key = str(self.phase) # either 'TRAIN' or 'TEST'
+        #cfg_key = 'TEST'
+        pre_nms_topN  = cfg[cfg_key].RPN_PRE_NMS_TOP_N
+        post_nms_topN = cfg[cfg_key].RPN_POST_NMS_TOP_N
+        nms_thresh    = cfg[cfg_key].RPN_NMS_THRESH
+        min_size      = cfg[cfg_key].RPN_MIN_SIZE
+
+        # the first set of _num_anchors channels are bg probs
+        # the second set are the fg probs, which we want
+        scores = rpn_cls_prob_reshape[:, _num_anchors:, :, :]
+        bbox_deltas = rpn_bbox_pred
+        #im_info = bottom[2].data[0, :]
+
+        if self.DEBUG:
+            print 'im_size: ({}, {})'.format(im_info[0], im_info[1])
+            print 'scale: {}'.format(im_info[2])
+
+        # 1. Generate proposals from bbox deltas and shifted anchors
+        height, width = scores.shape[-2:]
+
+        if self.DEBUG:
+            print 'score map size: {}'.format(scores.shape)
+
+        # Enumerate all shifts
+        shift_x = np.arange(0, width) * _feat_stride
+        shift_y = np.arange(0, height) * _feat_stride
+        shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+        shifts = np.vstack((shift_x.ravel(), shift_y.ravel(),
+                            shift_x.ravel(), shift_y.ravel())).transpose()
+
+        # Enumerate all shifted anchors:
+        #
+        # add A anchors (1, A, 4) to
+        # cell K shifts (K, 1, 4) to get
+        # shift anchors (K, A, 4)
+        # reshape to (K*A, 4) shifted anchors
+        A = _num_anchors
+        K = shifts.shape[0]
+        anchors = _anchors.reshape((1, A, 4)) + \
+                  shifts.reshape((1, K, 4)).transpose((1, 0, 2))
+        anchors = anchors.reshape((K * A, 4))
+
+        # Transpose and reshape predicted bbox transformations to get them
+        # into the same order as the anchors:
+        #
+        # bbox deltas will be (1, 4 * A, H, W) format
+        # transpose to (1, H, W, 4 * A)
+        # reshape to (1 * H * W * A, 4) where rows are ordered by (h, w, a)
+        # in slowest to fastest order
+        bbox_deltas = bbox_deltas.transpose((0, 2, 3, 1)).reshape((-1, 4))
+
+        # Same story for the scores:
+        #
+        # scores are (1, A, H, W) format
+        # transpose to (1, H, W, A)
+        # reshape to (1 * H * W * A, 1) where rows are ordered by (h, w, a)
+        scores = scores.transpose((0, 2, 3, 1)).reshape((-1, 1))
+
+        # Convert anchors into proposals via bbox transformations
+        proposals = bbox_transform_inv(anchors, bbox_deltas)
+
+        # 2. clip predicted boxes to image
+        proposals = clip_boxes(proposals, im_info[:2])
+
+        # 3. remove predicted boxes with either height or width < threshold
+        # (NOTE: convert min_size to input image scale stored in im_info[2])
+        keep = _filter_boxes(proposals, min_size * im_info[2])
+        proposals = proposals[keep, :]
+        scores = scores[keep]
+
+        # 4. sort all (proposal, score) pairs by score from highest to lowest
+        # 5. take top pre_nms_topN (e.g. 6000)
+        order = scores.ravel().argsort()[::-1]
+        if pre_nms_topN > 0:
+            order = order[:pre_nms_topN]
+        proposals = proposals[order, :]
+        scores = scores[order]
+
+        # 6. apply nms (e.g. threshold = 0.7)
+        # 7. take after_nms_topN (e.g. 300)
+        # 8. return the top proposals (-> RoIs top)
+        keep = nms(np.hstack((proposals, scores)), nms_thresh)
+        if post_nms_topN > 0:
+            keep = keep[:post_nms_topN]
+        proposals = proposals[keep, :]
+        scores = scores[keep]
+        # Output rois blob
+        # Our RPN implementation only supports a single input image, so all
+        # batch inds are 0
+        batch_inds = np.zeros((proposals.shape[0], 1), dtype=np.float32)
+        blob = np.hstack((batch_inds, proposals.astype(np.float32, copy=False)))
+
+        return blob
+
+
 
--- lib/tests/test_layer_rpn.py
+++ lib/tests/test_layer_rpn.py
@@ -14,6 +14,45 @@ class Test_layer_rpn(unittest.TestCase):
     def setUp(self):
         self.datadir = 'tests/test_layer_rpn_data/'
 
+    def test_rpn_head(self):
+        tf.reset_default_graph()
+        with open(self.datadir+'test_rpn_head.pkl', 'rb') as fid:
+            df = pickle.load(fid)
+
+        reshapein, out1_ref, out2_ref, w0, b0,w1,b1,w2,b2 = tuple(df)
+
+        input_tensor = tf.placeholder(dtype = tf.float32, shape = (None,None,None,512))
+        rpn = RPN(DEBUG = True)
+        rpn_cls_score, rpn_bbox_pred = rpn.rpn_head(input_tensor, nfilters = 512)
+
+        with tf.Session() as sess:
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_conv/3x3/kernel:0'),
+                w0))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_conv/3x3/bias:0'),
+                b0))
+
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_cls_score/kernel:0'),
+                w1))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_cls_score/bias:0'),
+                b1))
+
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_bbox_pred/kernel:0'),
+                w2))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_bbox_pred/bias:0'),
+                b2))
+
+            out1, out2 = sess.run([ rpn_cls_score , rpn_bbox_pred],
+                    feed_dict = {input_tensor : reshapein})
+
+        assert( np.equal(out1, out1_ref).all() )
+        assert( np.equal(out2, out2_ref).all() )
+
 
     def test_rpn_anchor_target_layer(self):
         tf.reset_default_graph()
@@ -61,19 +100,135 @@ class Test_layer_rpn(unittest.TestCase):
 
         rpn = RPN(DEBUG = True)
         input_tensor = tf.placeholder( dtype = tf.float32, shape = (None, None, None, None) )
-        rpn_cls_prob_reshape = rpn._rpn_cls_score_reshape(input_tensor)
+        rpn_cls_prob_reshape = rpn.rpn_cls_score_reshape(input_tensor)
 
         with tf.Session() as sess:
             res = sess.run(rpn_cls_prob_reshape, feed_dict = {input_tensor:rpn_cls_score})
+        assert( np.equal(res[1], res_ref).all() )
+
+
+    def test_rpn_loss(self):
+        tf.reset_default_graph()
+        with open(self.datadir+'test_rpn_loss.pkl', 'rb') as fid:
+            df = pickle.load(fid)
+        rpn_cls_score, rpn_bbox_pred, rpn_labels,\
+        rpn_bbox_targets, rpn_bbox_inside_weights, \
+        rpn_bbox_outside_weights, cross_entropy_ref, bbox_loss_ref = df
+
+        rpn = RPN(DEBUG = True)
+        cross_entropy, bbox_loss = rpn.rpn_loss( rpn_cls_score,
+                rpn_bbox_pred,
+                rpn_labels,
+                rpn_bbox_targets,
+                rpn_bbox_inside_weights,
+                rpn_bbox_outside_weights )
+
+        with tf.Session() as sess:
+            cross_entropy_res, bbox_loss_res = sess.run( [cross_entropy, bbox_loss] )
+
+        assert( np.equal(cross_entropy_ref, cross_entropy_res).all() )
+        assert( np.equal(bbox_loss_ref, bbox_loss_res).all() )
+
+
+    def test_proposal_layer(self):
+        tf.reset_default_graph()
+        with open(self.datadir+'test_proposal_layer.pkl', 'rb') as fid:
+            df = pickle.load(fid)
+        rpn_cls_prob_reshape = df['rpn_cls_prob_reshape']
+        rpn_bbox_pred = df['rpn_bbox_pred']
+        im_info = df['im_info']
+        cfg_key = df['cfg_key']
+        res_ref = df['res']
+
+        rpn = RPN(DEBUG = True)
+        blobs = rpn.proposal_layer(
+            rpn_cls_prob_reshape,
+            rpn_bbox_pred,
+            im_info,
+            cfg_key,
+            _feat_stride = [16,],
+            anchor_scales = [8, 16, 32]
+            )
+
+        with tf.Session() as sess:
+            res = sess.run(blobs)
 
         assert( np.equal(res, res_ref).all() )
 
 
+    def test_create(self):
+
+        tf.reset_default_graph()
+        input_tensor = tf.placeholder(dtype = tf.float32, shape = (None, None, None, 512))
+        im_info = tf.placeholder( dtype = tf.float32, shape = [None,None] )
+        gt_boxes = tf.placeholder( dtype = tf.float32, shape = [None, None] )
+
+        rpn = RPN(DEBUG = True).create( input_tensor, im_info, gt_boxes, isTrain = True )
+
+    def test_all(self):
+        tf.reset_default_graph()
+        with open(self.datadir+'test_rpn_all.pkl', 'rb') as fid:
+            df = pickle.load(fid)
+
+        features, im_info, gt, \
+        proposals_ref, rpn_cross_entropy_ref, rpn_loss_box_ref, \
+        w0,b0,w1,b1,w2,b2 = df
+
+        input_tensor = tf.placeholder(dtype = tf.float32, shape = (None, None, None, 512))
+        im_info_tensor= tf.placeholder( dtype = tf.float32, shape = [None,None] )
+        gt_boxes_tensor = tf.placeholder( dtype = tf.float32, shape = [None, None] )
+
+        rpn = RPN(DEBUG = True).create( input_tensor, im_info_tensor,
+                gt_boxes_tensor, isTrain = True )
+
+        proposal, cross_entro, bbox_loss = rpn.get_outputs()
+
+        with tf.Session() as sess:
+
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_conv/3x3/kernel:0'),
+                w0))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_conv/3x3/bias:0'),
+                b0))
+
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_cls_score/kernel:0'),
+                w1))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_cls_score/bias:0'),
+                b1))
+
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_bbox_pred/kernel:0'),
+                w2))
+            sess.run(tf.assign(
+                tf.get_default_graph().get_tensor_by_name('rpn_bbox_pred/bias:0'),
+                b2))
+
+            proposal_r, cross_entro_r, bbox_loss_r = sess.run( [proposal, cross_entro,
+                bbox_loss ] ,
+                feed_dict = {
+                    input_tensor : features,
+                    im_info_tensor: im_info,
+                    gt_boxes_tensor: gt
+                    })
+
+        assert( np.equal(proposals_ref, proposal_r).all() )
+        assert( np.equal(cross_entro_r, rpn_cross_entropy_ref).all() )
+        assert( np.equal(bbox_loss_r, rpn_loss_box_ref).all() )
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
-    #suite.addTest(Test_layer_rpn('test_rpn_anchor_target_layer'))
+
+    suite.addTest(Test_layer_rpn('test_rpn_head'))
+    suite.addTest(Test_layer_rpn('test_rpn_anchor_target_layer'))
     suite.addTest(Test_layer_rpn('test_rpn_cls_score_reshpae'))
+    suite.addTest(Test_layer_rpn('test_rpn_loss'))
+    suite.addTest(Test_layer_rpn('test_proposal_layer'))
+    suite.addTest(Test_layer_rpn('test_create'))
+    suite.addTest(Test_layer_rpn('test_all'))
 
     unittest.TextTestRunner(verbosity = 2).run(suite)
 
